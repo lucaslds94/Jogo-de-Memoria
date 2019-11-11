0496a0fddb7d1f090c8e105bcd64fc1c8a7ab5ca
Lucas Lima da Silva <lucas.lima@digisystem.com.br>
2019-11-10 21:00:46 -0300

    Corrigir bug da ultima carta nao aparecer antes do alerta

--- index.js
+++ index.js
@@ -15,15 +15,16 @@ function shuffle(images) {
     let currentIndex = images.length;
     let randomIndex;
     let temporaryValue;
-  
+
+
     while (0 !== currentIndex) {
-  
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      temporaryValue = images[currentIndex];
-      images[currentIndex] = images[randomIndex];
-      images[randomIndex] = temporaryValue;
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = images[currentIndex];
+        images[currentIndex] = images[randomIndex];
+        images[randomIndex] = temporaryValue;
     }
 }
 
@@ -33,7 +34,7 @@ shuffle(images);
 let cardHTML = '';
 
 images.forEach(img => {
-    cardHTML +=`
+    cardHTML += `
     <div class="memory-card">
     <img class="front-face" src="img/${img}">
     <img class="back-face"src="img/verso.jpg">
@@ -45,47 +46,46 @@ cardBoard.innerHTML = cardHTML;
 
 const cards = document.querySelectorAll('.memory-card');
 
-function flipCard(){
+function flipCard() {
     let cardOne, cardTwo;
 
     this.classList.add("flip");
     this.classList.add("active")
-    
+
     let flips = document.querySelectorAll('.flip')
     let elCards = document.querySelectorAll('.active')
 
- function ifDelayed() {
-    if(elCards.length == 2){
-        cardOne = elCards[0].children[0].attributes[1].nodeValue
-        cardTwo = elCards[1].children[0].attributes[1].nodeValue
+    function ifDelayed() {
+        if (elCards.length == 2) {
+            cardOne = elCards[0].children[0].attributes[1].nodeValue
+            cardTwo = elCards[1].children[0].attributes[1].nodeValue
 
-       if(cardOne === cardTwo){
-            elCards[0].classList.remove('active')
-            elCards[1].classList.remove('active')
-            console.log('Acertou')
-        } else {            
-            console.log('errou')
-            elCards[0].classList.remove('active')
-            elCards[1].classList.remove('active')
-            elCards[1].classList.remove('flip')
-            elCards[0].classList.remove('flip')
+            if (cardOne === cardTwo) {
+                elCards[0].classList.remove('active')
+                elCards[1].classList.remove('active')
+                console.log('Acertou')
+            } else {
+                console.log('errou')
+                elCards[0].classList.remove('active')
+                elCards[1].classList.remove('active')
+                elCards[1].classList.remove('flip')
+                elCards[0].classList.remove('flip')
+            }
         }
     }
-}
     setTimeout(ifDelayed, 400);
-    
-    function winMsg(){
-    if(flips.length === 8) {
-       alert("Voce ganhou")
+
+    function winTest() {
+        if (flips.length === 8) {
+            alert("Voce ganhou")
+        }
     }
-}
-winMsg()
-//setinterval(winMsg(), 400) 
-//console.log(elCard)
+    setTimeout(winTest, 400);
+    //console.log(elCard)
 }
 
 function verifica(cardOne, cardTwo) {
-    if(cardOne === cardTwo){
+    if (cardOne === cardTwo) {
         return true
     } else {
         return false
